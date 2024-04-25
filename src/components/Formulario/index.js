b628b6b78d26c8ff8a5975b17c4dc9e4406979bf
Nicolas Renaux <nickrenaux@gmail.com>
2024-04-24 21:48:27 -0300

    Evoluindo ainda mais o formulário, adicionando um select

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,6 +1,13 @@
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 const Formulario = () =>{
+
+    const times = [
+        'Front-End', 'Programação', 'Data Science', 'Back-End', 'Devops', 'Ux e Design',
+        'Mobile', 'Inovacao e Gestão'
+    ]
+
     return (
         <section className='formulario'>
             <form>
@@ -8,6 +15,7 @@ const Formulario = () =>{
             <CampoTexto label="Nome" placeholder="Digite seu nome"/>
             <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
             <CampoTexto label="Imagem" placeholder="Insira o endereço da imagem"/>
+            <ListaSuspensa label="Time" itens = {times}/>
             </form>
         </section>
     )
